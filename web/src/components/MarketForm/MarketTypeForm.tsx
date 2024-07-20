985790bd268985ddf4ebc8647339a96f8b984e07
xyzseer <xyzseer@gmail.com>
2024-07-21 01:00:15 +0200

    add market types explanation

--- web/src/components/MarketForm/MarketTypeForm.tsx
+++ web/src/components/MarketForm/MarketTypeForm.tsx
@@ -1,7 +1,19 @@
 import { MarketTypes } from "@/lib/market";
 import { FormProvider } from "react-hook-form";
 import { ButtonsWrapper, FormStepProps, FormWithNextStep, MarketTypeFormValues } from ".";
+import { Alert } from "../Alert";
 import Select from "../Form/Select";
+
+const MARKETS_DESCRIPTIONS = {
+  [MarketTypes.CATEGORICAL]: `<p>Questions with one specific answer.</p>
+  <p class="font-medium">Example: Which party wins 2024 US Presidential Election?</p>`,
+  [MarketTypes.SCALAR]: `<p>Questions with two outcomes that represent a minimum (“lower”) and a maxim (“upper”) range of a continuous scale, where the answer is represented by a numerical value that can fall either within or outside the margins determined by the outcomes.</p>
+  <p class="font-medium">Example: What will ETH/USD price be at the end of December 2024?</p>`,
+  [MarketTypes.MULTI_CATEGORICAL]: `<p>Questions that may have more than one answer.</p>
+  <p class="font-medium">Example: Which countries will win the most medals at the 2024 Paris Olympics?</p>`,
+  [MarketTypes.MULTI_SCALAR]: `<p>Questions with outcomes that are measured on multiple continuous scales, where each outcome has its own question and an answer represented by a numerical value.</p>
+  <p class="font-medium">Example: How many seats will each group get in the EU elections?</p>`,
+};
 
 export function MarketTypeForm({
   useFormReturn,
@@ -11,6 +23,7 @@ export function MarketTypeForm({
     register,
     formState: { isValid },
     handleSubmit,
+    watch,
   } = useFormReturn;
 
   const MARKET_TYPES_OPTIONS = [
@@ -20,6 +33,8 @@ export function MarketTypeForm({
     { value: MarketTypes.MULTI_CATEGORICAL, text: "Multi Categorical" },
     { value: MarketTypes.MULTI_SCALAR, text: "Multi Scalar" },
   ];
+
+  const marketType = watch("marketType");
 
   return (
     <FormProvider {...useFormReturn}>
@@ -35,6 +50,15 @@ export function MarketTypeForm({
             useFormReturn={useFormReturn}
           />
         </div>
+
+        {MARKETS_DESCRIPTIONS[marketType] && (
+          <Alert type="info">
+            <div
+              dangerouslySetInnerHTML={{ __html: MARKETS_DESCRIPTIONS[marketType] }}
+              className="space-y-[10px]"
+            ></div>
+          </Alert>
+        )}
 
         <ButtonsWrapper goToNextStep={goToNextStep} disabled={!isValid} />
       </form>
