691e5de6ef73a5db5aac3d6c39b179214fba67d4
nvm1410 <vominhbinto@gmail.com>
2024-08-06 10:17:01 +0700

    fix: bug in default sort

--- web/src/hooks/useSortMarket.ts
+++ web/src/hooks/useSortMarket.ts
@@ -11,6 +11,12 @@ const statusPriority = {
   not_verified: 2,
 };
 
+type ExtendedMarket = Market & {
+  creator?: string | null;
+  outcomeAddresses?: `0x${string}`[];
+  liquidity?: bigint;
+};
+
 function useSortMarket(markets: Market[]) {
   const { address: currentUserAddress, chainId = DEFAULT_CHAIN } = useAccount();
   const { data: pools = [] } = useAllOutcomePools(chainId as SupportedChain);
@@ -23,43 +29,36 @@ function useSortMarket(markets: Market[]) {
     },
     {} as { [key: string]: bigint },
   );
-  return markets.sort(
-    (
-      a: Market & {
-        creator?: string | null;
-        outcomeAddresses?: `0x${string}`[];
-      },
-      b: Market & {
-        creator?: string | null;
-        outcomeAddresses?: `0x${string}`[];
-      },
-    ) => {
-      // markets created by current user will show first
-      const isACurrentUser = a.creator?.toLowerCase() === currentUserAddress?.toLowerCase();
-      const isBCurrentUser = b.creator?.toLowerCase() === currentUserAddress?.toLowerCase();
+  return structuredClone(
+    markets.map((market: ExtendedMarket) => {
+      return {
+        ...market,
+        liquidity: bigIntMax(
+          ...(market.outcomeAddresses?.map((address) => outcomeLiquidityMapping[address.toLowerCase()]) ?? []),
+        ),
+      };
+    }),
+  ).sort((a: ExtendedMarket, b: ExtendedMarket) => {
+    // markets created by current user will show first
+    const isACurrentUser = a.creator?.toLowerCase() === currentUserAddress?.toLowerCase();
+    const isBCurrentUser = b.creator?.toLowerCase() === currentUserAddress?.toLowerCase();
 
-      // If one is created by current user and the other isn't, prioritize the current user's item
-      if (isACurrentUser && !isBCurrentUser) return -1;
-      if (!isACurrentUser && isBCurrentUser) return 1;
+    // If one is created by current user and the other isn't, prioritize the current user's item
+    if (isACurrentUser && !isBCurrentUser) return -1;
+    if (!isACurrentUser && isBCurrentUser) return 1;
 
-      //by verification status
-      const verificationStatusA = verificationStatusResultList?.[a.id.toLowerCase()] ?? defaultStatus;
-      const verificationStatusB = verificationStatusResultList?.[b.id.toLowerCase()] ?? defaultStatus;
-      const statusDiff = statusPriority[verificationStatusA.status] - statusPriority[verificationStatusB.status];
-      if (statusDiff !== 0) {
-        return statusDiff;
-      }
+    //by verification status
+    const verificationStatusA = verificationStatusResultList?.[a.id.toLowerCase()] ?? defaultStatus;
+    const verificationStatusB = verificationStatusResultList?.[b.id.toLowerCase()] ?? defaultStatus;
+    const statusDiff = statusPriority[verificationStatusA.status] - statusPriority[verificationStatusB.status];
+    if (statusDiff !== 0) {
+      return statusDiff;
+    }
 
-      // by liquidity
-      const marketALiquidity = bigIntMax(
-        ...(a.outcomeAddresses?.map((address) => outcomeLiquidityMapping[address.toLowerCase()]) ?? []),
-      );
-      const marketBLiquidity = bigIntMax(
-        ...(b.outcomeAddresses?.map((address) => outcomeLiquidityMapping[address.toLowerCase()]) ?? []),
-      );
-      return marketALiquidity === marketBLiquidity ? 0 : marketBLiquidity > marketALiquidity ? 1 : -1;
-    },
-  );
+    // by liquidity
+
+    return a.liquidity === b.liquidity ? 0 : (b.liquidity ?? 0n) > (a.liquidity ?? 0n) ? 1 : -1;
+  });
 }
 
 export default useSortMarket;
