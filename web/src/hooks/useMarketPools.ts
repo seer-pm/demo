a8cd26d1caac20beb94bd9804b8e765c5f536466
xyzseer <xyzseer@gmail.com>
2024-09-06 13:26:42 -0300

    batch pool queries

--- web/src/hooks/useMarketPools.ts
+++ web/src/hooks/useMarketPools.ts
@@ -3,6 +3,8 @@ import { COLLATERAL_TOKENS } from "@/lib/config";
 import { swaprGraphQLClient } from "@/lib/subgraph";
 import { isUndefined } from "@/lib/utils";
 import { useQuery } from "@tanstack/react-query";
+import * as batshit from "@yornaath/batshit";
+import memoize from "micro-memoize";
 import { Address, formatUnits } from "viem";
 import {
   GetDepositsQuery,
@@ -50,32 +52,68 @@ function mapEternalFarming(eternalFarming: GetEternalFarmingsQuery["eternalFarmi
   };
 }
 
+const eternalFarming = memoize((chainId: SupportedChain) => {
+  return batshit.create({
+    name: "eternalFarmings",
+    fetcher: async (ids: Address[]) => {
+      const algebraFarmingClient = swaprGraphQLClient(chainId, "algebrafarming");
+
+      if (!algebraFarmingClient) {
+        throw new Error("Subgraph not available");
+      }
+
+      const { eternalFarmings } = await getSdk(algebraFarmingClient).GetEternalFarmings({
+        where: { pool_in: ids },
+      });
+
+      return eternalFarmings;
+    },
+    scheduler: batshit.windowScheduler(10),
+    resolver: (eternalFarmings, poolId) => eternalFarmings.filter((eternalFarming) => eternalFarming.pool === poolId),
+  });
+});
+
+const getPools = memoize((chainId: SupportedChain) => {
+  return batshit.create({
+    name: "getPools",
+    fetcher: async (tokens: { token0: Address; token1: Address }[]) => {
+      const algebraClient = swaprGraphQLClient(chainId, "algebra");
+
+      if (!algebraClient) {
+        throw new Error("Subgraph not available");
+      }
+
+      const { pools } = await getSdk(algebraClient).GetPools({
+        where: {
+          or: tokens.map((t) => ({ token0: t.token0.toLocaleLowerCase(), token1: t.token1.toLocaleLowerCase() })),
+        },
+        orderBy: Pool_OrderBy.TotalValueLockedUsd,
+        orderDirection: OrderDirection.Desc,
+      });
+
+      return pools;
+    },
+    scheduler: batshit.windowScheduler(10),
+    resolver: (pools, tokens) =>
+      pools.filter(
+        (p) => p.token0.id === tokens.token0.toLocaleLowerCase() && p.token1.id === tokens.token1.toLocaleLowerCase(),
+      ),
+  });
+});
+
 async function getPoolInfo(
   chainId: SupportedChain,
   outcomeToken: Address,
   collateralToken: Address,
 ): Promise<PoolInfo[]> {
-  const algebraClient = swaprGraphQLClient(chainId, "algebra");
-  const algebraFarmingClient = swaprGraphQLClient(chainId, "algebrafarming");
-
-  if (!algebraClient || !algebraFarmingClient) {
-    throw new Error("Subgraph not available");
-  }
-
   const [token0, token1] =
     outcomeToken > collateralToken ? [collateralToken, outcomeToken] : [outcomeToken, collateralToken];
 
-  const { pools } = await getSdk(algebraClient).GetPools({
-    where: { token0: token0.toLocaleLowerCase(), token1: token1.toLocaleLowerCase() },
-    orderBy: Pool_OrderBy.TotalValueLockedUsd,
-    orderDirection: OrderDirection.Desc,
-  });
+  const pools = await getPools(chainId).fetch({ token0, token1 });
 
   return await Promise.all(
     pools.map(async (pool) => {
-      const { eternalFarmings } = await getSdk(algebraFarmingClient).GetEternalFarmings({
-        where: { pool: pool.id as Address },
-      });
+      const eternalFarmings = await eternalFarming(chainId).fetch(pool.id as Address);
 
       return {
         id: pool.id as Address,
