dcb950e955e5942c65e274be71a692e987da654a
nvm1410 <vominhbinto@gmail.com>
2024-11-22 16:32:13 +0700

    fix: hide orbris connect button

--- web/src/components/Market/MarketTabs/Comments.tsx
+++ web/src/components/Market/MarketTabs/Comments.tsx
@@ -1,27 +1,29 @@
 import { useLocalStorageKey } from "@/hooks/useLocalStorageKey";
 import { Market } from "@/hooks/useMarket";
+import { config } from "@/wagmi";
 import { Discussion } from "@orbisclub/components";
 import "@orbisclub/components/dist/index.modern.css";
-import { config } from "@/wagmi";
 import { disconnect } from "@wagmi/core";
 import { useEffect } from "react";
 import { useAccount } from "wagmi";
 
 function Comments({ market }: { market: Market }) {
   const { isConnected } = useAccount();
-  useEffect(() => {
-    const orbisContainer = document.querySelector("._MBDTd");
-    if (!orbisContainer) return;
-    const loginContainer = orbisContainer.children[0] as HTMLDivElement;
-    console.log(loginContainer);
-    if (!loginContainer) return;
-    loginContainer.style.display = isConnected ? "block" : "none";
-  }, [isConnected]);
+
   const ceramicSession = useLocalStorageKey("ceramic-session", (value) => {
     if (!value && isConnected) {
       disconnect(config);
     }
   });
+
+  useEffect(() => {
+    const orbisContainer = document.querySelector("._MBDTd");
+    if (!orbisContainer) return;
+    const loginContainer = orbisContainer.children[0] as HTMLDivElement;
+    if (!loginContainer) return;
+    loginContainer.style.display = ceramicSession ? "block" : "none";
+  }, [ceramicSession]);
+
   return (
     <Discussion key={ceramicSession} context={`${import.meta.env.VITE_ORBIS_CONTEXT}:${market.id.toLowerCase()}`} />
   );
