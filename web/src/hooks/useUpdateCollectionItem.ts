865e31fa32ae142a5935565cf4e27b0fd07367a1
nvm1410 <vominhbinto@gmail.com>
2025-01-22 11:05:51 +0700

    fix: optimistic update favorite

--- web/src/hooks/useUpdateCollectionItem.ts
+++ web/src/hooks/useUpdateCollectionItem.ts
@@ -10,7 +10,7 @@ interface UpdateCollectionItemProps {
 }
 
 export async function updateCollectionItem(props: UpdateCollectionItemProps): Promise<void> {
-  fetchAuth(props.accessToken, "/.netlify/functions/collections", "POST", {
+  await fetchAuth(props.accessToken, "/.netlify/functions/collections", "POST", {
     marketIds: props.marketIds,
     collectionId: props.collectionId,
   });
@@ -20,8 +20,35 @@ export const useUpdateCollectionItem = (onSuccess?: () => void) => {
   return useMutation({
     mutationKey: ["useUpdateCollectionItem"],
     mutationFn: updateCollectionItem,
-    onSuccess: () => {
+    onMutate: async (newProps) => {
+      await queryClient.cancelQueries({ queryKey: ["useFavorites"] });
+
+      // Snapshot the previous value
+      const previousFavorites = queryClient.getQueryData(["useFavorites"]);
+
+      // Optimistically update to the new value
+      queryClient.setQueryData(["useFavorites"], (old: Address[]) => {
+        const marketId = newProps.marketIds[0];
+        if (old.includes(marketId)) {
+          return old.filter((x) => x !== marketId);
+        }
+        return [...old, marketId];
+      });
+      // Return a context object with the snapshotted value
+      return { previousFavorites };
+    },
+    // If the mutation fails,
+    // use the context returned from onMutate to roll back
+    onError: (_err, _newProps, context) => {
+      if (context?.previousFavorites) {
+        queryClient.setQueryData(["useFavorites"], context?.previousFavorites);
+      }
+    },
+    // Always refetch after error or success:
+    onSettled: () => {
       queryClient.invalidateQueries({ queryKey: ["useFavorites"] });
+    },
+    onSuccess: () => {
       onSuccess?.();
     },
   });
