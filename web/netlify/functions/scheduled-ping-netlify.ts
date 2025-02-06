3504a9be82876781b2984ab9b943b6e6d90573f2
nvm1410 <vominhbinto@gmail.com>
2025-02-06 22:50:57 +0700

    fix: try to optimize first time homepage load time

--- web/netlify/functions/scheduled-ping-netlify.ts
+++ web/netlify/functions/scheduled-ping-netlify.ts
@@ -2,35 +2,8 @@ import { getAppUrl } from "@/lib/utils";
 import { Config } from "@netlify/functions";
 
 export default async () => {
-  // ping markets-search and market-metadata endpoints
   try {
-    await Promise.all([
-      fetch(`${getAppUrl()}/.netlify/functions/markets-search`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          chainsList: [],
-          marketName: "",
-          marketStatusList: [],
-          creator: "",
-          participant: "",
-          orderBy: undefined,
-          orderDirection: undefined,
-        }),
-      }),
-      fetch(`${getAppUrl()}/.netlify/functions/market-metadata`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          chainsList: ["100"],
-          id: "0x07c5a013b44e0dbf72a30b45f13af1e9dbb3970d",
-        }),
-      }),
-    ]);
+    await fetch(getAppUrl());
   } catch (error) {
     console.error("Error ping netlify:", error);
     throw error;
