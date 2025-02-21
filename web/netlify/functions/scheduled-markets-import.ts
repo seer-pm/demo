068594d27c81149b674d5c9b13010e3b5dc99385
xyzseer <xyzseer@gmail.com>
2025-02-21 17:26:24 -0300

    store and search markets on supabase

---
+++ web/netlify/functions/scheduled-markets-import.ts
@@ -0,0 +1,140 @@
+import { lightGeneralizedTcrAddress } from "@/hooks/contracts/generated.ts";
+import { VerificationResult } from "@/hooks/useMarket.ts";
+import { SupportedChain } from "@/lib/chains.ts";
+import { isUndefined } from "@/lib/utils.ts";
+import { Config } from "@netlify/functions";
+import { createClient } from "@supabase/supabase-js";
+import { Address } from "viem";
+import { Status, getSdk as getCurateSdk } from "../../src/hooks/queries/gql-generated-curate";
+import { chainIds } from "./utils/config.ts";
+import { curateGraphQLClient, getSubgraphUrl } from "./utils/subgraph.ts";
+
+const supabase = createClient(process.env.VITE_SUPABASE_PROJECT_URL!, process.env.VITE_SUPABASE_API_KEY!);
+
+async function getVerificationStatusList(
+  chainId: SupportedChain,
+): Promise<Record<Address, VerificationResult | undefined>> {
+  const client = curateGraphQLClient(chainId);
+
+  const registryAddress = lightGeneralizedTcrAddress[chainId];
+  if (client && !isUndefined(registryAddress)) {
+    const { litems } = await getCurateSdk(client).GetImages({
+      where: {
+        registryAddress,
+      },
+    });
+    return litems.reduce(
+      (obj, item) => {
+        const marketId = item.metadata?.props?.find((prop) => prop.label === "Market")?.value?.toLowerCase();
+        if (!marketId) {
+          return obj;
+        }
+        const isVerifiedBeforeClearing =
+          item.status === Status.ClearingRequested &&
+          item.requests.find((request) => request.requestType === Status.RegistrationRequested)?.resolved;
+        if (item.status === Status.Registered || isVerifiedBeforeClearing) {
+          obj[marketId] = { status: "verified", itemID: item.itemID };
+          return obj;
+        }
+        if (item.status === Status.RegistrationRequested) {
+          if (item.disputed) {
+            obj[marketId] = { status: "challenged", itemID: item.itemID };
+          } else {
+            obj[marketId] = { status: "verifying", itemID: item.itemID };
+          }
+          return obj;
+        }
+        obj[marketId] = { status: "not_verified" };
+        return obj;
+      },
+      {} as { [key: string]: VerificationResult },
+    );
+  }
+
+  return {};
+}
+
+async function processChain(chainId: SupportedChain) {
+  const response = await fetch(getSubgraphUrl("seer", chainId), {
+    method: "POST",
+    body: JSON.stringify({
+      query: `{
+        markets(first: 1000) {
+          id
+          type
+          marketName
+          outcomes
+          wrappedTokens
+          collateralToken
+          collateralToken1
+          collateralToken2
+          parentMarket {
+            id
+            payoutReported
+            conditionId
+            payoutNumerators
+          }
+          parentOutcome
+          parentCollectionId
+          conditionId
+          questionId
+          templateId
+          questions {
+            question {
+              id
+              arbitrator
+              opening_ts
+              timeout
+              finalize_ts
+              is_pending_arbitration
+              best_answer
+              bond
+              min_bond
+            }
+          }
+          openingTs
+          finalizeTs
+          encodedQuestions
+          lowerBound
+          upperBound
+          payoutReported
+          payoutNumerators
+          factory
+          creator
+          outcomesSupply
+          blockTimestamp
+        }
+      }`,
+    }),
+  });
+  const {
+    data: { markets },
+  } = await response.json();
+
+  if (markets.length === 0) {
+    return;
+  }
+
+  const verificationStatusList = await getVerificationStatusList(chainId);
+
+  await supabase.from("markets").upsert(
+    markets.map((market) => ({
+      id: market.id,
+      chain_id: chainId,
+      subgraph_data: market,
+      verification: verificationStatusList[market.id] ?? {
+        status: "not_verified",
+      },
+    })),
+  );
+}
+
+export default async () => {
+  for (const chainId of chainIds) {
+    await processChain(chainId);
+  }
+};
+
+export const config: Config = {
+  schedule: "*/5 * * * *",
+};
---
+++ web/netlify/functions/utils/supabase.ts
@@ -0,0 +1,321 @@
+export type Json = string | number | boolean | null | { [key: string]: Json | undefined } | Json[];
+
+export type Database = {
+  graphql_public: {
+    Tables: {
+      [_ in never]: never;
+    };
+    Views: {
+      [_ in never]: never;
+    };
+    Functions: {
+      graphql: {
+        Args: {
+          operationName?: string;
+          query?: string;
+          variables?: Json;
+          extensions?: Json;
+        };
+        Returns: Json;
+      };
+    };
+    Enums: {
+      [_ in never]: never;
+    };
+    CompositeTypes: {
+      [_ in never]: never;
+    };
+  };
+  public: {
+    Tables: {
+      collections: {
+        Row: {
+          created_at: string | null;
+          id: number;
+          name: string;
+          updated_at: string | null;
+          url: string | null;
+          user_id: string;
+        };
+        Insert: {
+          created_at?: string | null;
+          id?: number;
+          name: string;
+          updated_at?: string | null;
+          url?: string | null;
+          user_id: string;
+        };
+        Update: {
+          created_at?: string | null;
+          id?: number;
+          name?: string;
+          updated_at?: string | null;
+          url?: string | null;
+          user_id?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "collections_user_id_fkey";
+            columns: ["user_id"];
+            isOneToOne: false;
+            referencedRelation: "users";
+            referencedColumns: ["id"];
+          },
+        ];
+      };
+      collections_markets: {
+        Row: {
+          collection_id: number | null;
+          id: number;
+          market_id: string;
+          user_id: string;
+        };
+        Insert: {
+          collection_id?: number | null;
+          id?: number;
+          market_id: string;
+          user_id: string;
+        };
+        Update: {
+          collection_id?: number | null;
+          id?: number;
+          market_id?: string;
+          user_id?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "collections_markets_collection_id_fkey";
+            columns: ["collection_id"];
+            isOneToOne: false;
+            referencedRelation: "collections";
+            referencedColumns: ["id"];
+          },
+          {
+            foreignKeyName: "favorite_markets_user_id_fkey";
+            columns: ["user_id"];
+            isOneToOne: false;
+            referencedRelation: "users";
+            referencedColumns: ["id"];
+          },
+        ];
+      };
+      key_value: {
+        Row: {
+          id: number;
+          key: string;
+          value: Json;
+        };
+        Insert: {
+          id?: number;
+          key: string;
+          value: Json;
+        };
+        Update: {
+          id?: number;
+          key?: string;
+          value?: Json;
+        };
+        Relationships: [];
+      };
+      markets: {
+        Row: {
+          categories: string[] | null;
+          chain_id: number | null;
+          created_at: string | null;
+          creator: string | null;
+          id: string;
+          incentive: number | null;
+          liquidity: number | null;
+          metadata: Json | null;
+          odds: number[] | null;
+          pool_balance: Json;
+          subgraph_data: Json | null;
+          updated_at: string | null;
+          url: string | null;
+          verification: Json | null;
+        };
+        Insert: {
+          categories?: string[] | null;
+          chain_id?: number | null;
+          created_at?: string | null;
+          creator?: string | null;
+          id: string;
+          incentive?: number | null;
+          liquidity?: number | null;
+          metadata?: Json | null;
+          odds?: number[] | null;
+          pool_balance?: Json;
+          subgraph_data?: Json | null;
+          updated_at?: string | null;
+          url?: string | null;
+          verification?: Json | null;
+        };
+        Update: {
+          categories?: string[] | null;
+          chain_id?: number | null;
+          created_at?: string | null;
+          creator?: string | null;
+          id?: string;
+          incentive?: number | null;
+          liquidity?: number | null;
+          metadata?: Json | null;
+          odds?: number[] | null;
+          pool_balance?: Json;
+          subgraph_data?: Json | null;
+          updated_at?: string | null;
+          url?: string | null;
+          verification?: Json | null;
+        };
+        Relationships: [];
+      };
+      notifications_queue: {
+        Row: {
+          created_at: string;
+          data: Json;
+          email: string;
+          event_id: string;
+          id: number;
+          sent_at: string | null;
+        };
+        Insert: {
+          created_at?: string;
+          data: Json;
+          email: string;
+          event_id: string;
+          id?: number;
+          sent_at?: string | null;
+        };
+        Update: {
+          created_at?: string;
+          data?: Json;
+          email?: string;
+          event_id?: string;
+          id?: number;
+          sent_at?: string | null;
+        };
+        Relationships: [];
+      };
+      users: {
+        Row: {
+          email: string;
+          email_verified: boolean;
+          id: string;
+          last_login_at: string | null;
+          verification_token: string | null;
+        };
+        Insert: {
+          email?: string;
+          email_verified?: boolean;
+          id: string;
+          last_login_at?: string | null;
+          verification_token?: string | null;
+        };
+        Update: {
+          email?: string;
+          email_verified?: boolean;
+          id?: string;
+          last_login_at?: string | null;
+          verification_token?: string | null;
+        };
+        Relationships: [];
+      };
+    };
+    Views: {
+      [_ in never]: never;
+    };
+    Functions: {
+      [_ in never]: never;
+    };
+    Enums: {
+      [_ in never]: never;
+    };
+    CompositeTypes: {
+      [_ in never]: never;
+    };
+  };
+};
+
+type PublicSchema = Database[Extract<keyof Database, "public">];
+
+export type Tables<
+  PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] & PublicSchema["Views"]) | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+        Database[PublicTableNameOrOptions["schema"]]["Views"])
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R;
+    }
+    ? R
+    : never
+  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] & PublicSchema["Views"])
+    ? (PublicSchema["Tables"] & PublicSchema["Views"])[PublicTableNameOrOptions] extends {
+        Row: infer R;
+      }
+      ? R
+      : never
+    : never;
+
+export type TablesInsert<
+  PublicTableNameOrOptions extends keyof PublicSchema["Tables"] | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I;
+    }
+    ? I
+    : never
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Insert: infer I;
+      }
+      ? I
+      : never
+    : never;
+
+export type TablesUpdate<
+  PublicTableNameOrOptions extends keyof PublicSchema["Tables"] | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U;
+    }
+    ? U
+    : never
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Update: infer U;
+      }
+      ? U
+      : never
+    : never;
+
+export type Enums<
+  PublicEnumNameOrOptions extends keyof PublicSchema["Enums"] | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+    : never = never,
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
+    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
+    : never;
+
+export type CompositeTypes<
+  PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"] | { schema: keyof Database },
+  CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
+    schema: keyof Database;
+  }
+    ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+    : never = never,
+> = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
+  : PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
+    ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+    : never;
