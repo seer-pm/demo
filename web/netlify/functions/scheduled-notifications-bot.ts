8343a7240a087ba68197a2d3a04cb672de1c28a6
xyzseer <xyzseer@gmail.com>
2025-03-20 03:03:47 -0300

    telegram notifications bot

---
+++ web/netlify/functions/scheduled-notifications-bot.ts
@@ -0,0 +1,177 @@
+import { realityAddress } from "@/hooks/contracts/generated.ts";
+import { Question } from "@/hooks/useMarket.ts";
+import { SupportedChain, sepolia } from "@/lib/chains.ts";
+import { decodeQuestion, getAnswerText, getRealityLink } from "@/lib/reality.ts";
+import { Config } from "@netlify/functions";
+import { createClient } from "@supabase/supabase-js";
+import { getBlockNumber } from "@wagmi/core";
+import TelegramBot from "node-telegram-bot-api";
+import { parseAbiItem } from "viem";
+import { getPublicClientForNetwork } from "./utils/common.ts";
+import { chainIds, config as wagmiConfig } from "./utils/config.ts";
+import { getLastProcessedBlock, updateLastProcessedBlock } from "./utils/logs.ts";
+
+const SEER_NOTIFICATIONS_CHANNEL = "-1002545711308";
+
+const supabase = createClient(process.env.VITE_SUPABASE_PROJECT_URL!, process.env.VITE_SUPABASE_API_KEY!);
+
+async function getTelegramBot(): Promise<TelegramBot | undefined> {
+  if (!process.env.TELEGRAM_NOTIFICATIONS_BOT_TOKEN) {
+    return;
+  }
+
+  const TelegramBot = require("node-telegram-bot-api");
+  const bot = new TelegramBot(process.env.TELEGRAM_NOTIFICATIONS_BOT_TOKEN);
+
+  return bot;
+}
+
+async function getMarketsFromQuestions(questionIdToAnswerMap: Record<string, string>) {
+  try {
+    const { data, error } = await supabase
+      .from("markets")
+      .select("id, url, subgraph_data")
+      .not("subgraph_data->questions", "is", null);
+
+    if (error) {
+      console.error("Error fetching markets from Supabase:", error);
+      return new Map();
+    }
+
+    const questionToMarketMap = new Map<
+      `0x${string}`,
+      {
+        marketId: string;
+        url: string;
+        encodedQuestion: string;
+        question: Question;
+        answer: string;
+        outcomes: string[];
+        templateId: string;
+      }
+    >();
+
+    for (const market of data) {
+      const questions = (market.subgraph_data?.questions || []) as { question: Question }[];
+      for (let i = 0; i < questions.length; i++) {
+        const question = questions[i];
+        if (question?.question?.id && questionIdToAnswerMap[question.question.id]) {
+          questionToMarketMap.set(question.question.id as `0x${string}`, {
+            marketId: market.id,
+            url: market.url,
+            encodedQuestion: market.subgraph_data.encodedQuestions[i],
+            question: question.question,
+            answer: questionIdToAnswerMap[question.question.id],
+            outcomes: market.subgraph_data.outcomes,
+            templateId: market.subgraph_data.templateId,
+          });
+        }
+      }
+    }
+
+    return questionToMarketMap;
+  } catch (error) {
+    console.error("Error processing market data:", error);
+    return new Map();
+  }
+}
+
+const REALITY_LOG_NEW_ANSWER_EVENT = parseAbiItem(
+  "event LogNewAnswer( bytes32 answer, bytes32 indexed question_id, bytes32 history_hash, address indexed user, uint256 bond, uint256 ts, bool is_commitment)",
+);
+// const REALITY_LOG_NOTIFY_OF_ARBITRATION_REQUEST = parseAbiItem(
+//   "event LogNotifyOfArbitrationRequest( bytes32 indexed question_id, address indexed user)"
+// );
+
+async function getNewAnswerEvents(chainId: SupportedChain, fromBlock: bigint) {
+  try {
+    // Listen for RealityETH LogNewAnswer events
+    const newAnswerLogs = await getPublicClientForNetwork(chainId).getLogs({
+      address: realityAddress[chainId],
+      event: REALITY_LOG_NEW_ANSWER_EVENT,
+      fromBlock,
+      toBlock: "latest",
+    });
+
+    console.log(`[Network ${chainId}] Found ${newAnswerLogs.length} new answer events`);
+
+    const questionIdToAnswerMap = newAnswerLogs.reduce(
+      (acc, { args }) => {
+        acc[args.question_id!] = args.answer!;
+        return acc;
+      },
+      {} as Record<string, string>,
+    );
+
+    const questionToMarketMap = await getMarketsFromQuestions(questionIdToAnswerMap);
+    console.log(`[Network ${chainId}] Filtered down to ${questionToMarketMap.size} market-related question events`);
+
+    return [...questionToMarketMap.values()];
+  } catch (error) {
+    console.error(`[Network ${chainId}] Error fetching answer events:`, error);
+    throw error;
+  }
+}
+
+function getLastProcessedBlockKey(chainId: SupportedChain): string {
+  return `reality-new-answer-events-${chainId}-last-block`;
+}
+
+async function processChain(chainId: SupportedChain, bot: TelegramBot) {
+  const fromBlock = await getLastProcessedBlock(chainId, getLastProcessedBlockKey(chainId));
+
+  const newAnswers = await getNewAnswerEvents(chainId, fromBlock);
+
+  if (newAnswers.length > 0) {
+    const messages = newAnswers.map((data) => {
+      const decodedQuestion = decodeQuestion(data.encodedQuestion);
+      const question = data.question;
+      question.best_answer = data.answer;
+
+      return `Question: ${decodedQuestion.question}\n
+Answer: ${getAnswerText(question, data.outcomes, BigInt(data.templateId))}\n
+<a href="${getRealityLink(chainId, data.question.id)}">Check on Reality</a>\n
+<a href="https://app.seer.pm/markets/${chainId}/${data.url}">Check on Seer</a>`;
+    });
+
+    // Send messages with a 2-second delay between each one
+    for (const message of messages) {
+      await bot.sendMessage(SEER_NOTIFICATIONS_CHANNEL, message, {
+        parse_mode: "HTML",
+      });
+
+      // Wait for 2 seconds before sending the next message
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+    }
+  }
+
+  const currentBlock = await getBlockNumber(wagmiConfig, {
+    chainId,
+  });
+  await updateLastProcessedBlock(chainId, currentBlock, getLastProcessedBlockKey(chainId));
+}
+
+export default async () => {
+  const bot = await getTelegramBot();
+
+  if (!bot) {
+    console.log("Missing TELEGRAM_NOTIFICATIONS_BOT_TOKEN variable");
+    return;
+  }
+
+  for (const chainId of chainIds) {
+    if (chainId === sepolia.id) {
+      // ignore testnets
+      continue;
+    }
+    try {
+      await processChain(chainId, bot);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+};
+
+export const config: Config = {
+  schedule: "*/10 * * * *",
+};
