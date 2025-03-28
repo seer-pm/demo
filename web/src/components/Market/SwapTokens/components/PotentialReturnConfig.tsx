b65a38a7ee83aa77dd6b2a9d779f9cda2f480313
nvm1410 <vominhbinto@gmail.com>
2025-03-28 11:30:31 +0700

    update scalar potential return ui

--- web/src/components/Market/SwapTokens/components/PotentialReturnConfig.tsx
+++ web/src/components/Market/SwapTokens/components/PotentialReturnConfig.tsx
@@ -9,6 +9,8 @@ import { useTokensInfo } from "@/hooks/useTokenInfo";
 import { MarketTypes, getMarketType } from "@/lib/market";
 import { Token, getCollateralPerShare, getPotentialReturn } from "@/lib/tokens";
 import { isTwoStringsEqual, isUndefined } from "@/lib/utils";
+import clsx from "clsx";
+import ReactECharts from "echarts-for-react";
 import { Dispatch, ReactNode, SetStateAction, useEffect, useRef, useState } from "react";
 import { formatUnits, zeroAddress } from "viem";
 import { PotentialReturnResult } from "./PotentialReturn";
@@ -118,7 +120,6 @@ function RenderInputByMarketType({
       multiScalar: [],
     });
   }, [outcomeText, market, setInput]);
-
   if (marketType === MarketTypes.MULTI_CATEGORICAL) {
     return (
       <div className="space-y-1">
@@ -143,8 +144,8 @@ function RenderInputByMarketType({
         <p className="whitespace-nowrap">Market resolution:</p>
         <Input
           type="number"
-          className="h-[30px] w-full"
-          value={input.scalar || ""}
+          className="w-full"
+          value={input.scalar ?? ""}
           onChange={(e) => setInput((state) => ({ ...state, scalar: Number(e.target.value) }))}
         />
       </div>
@@ -191,9 +192,10 @@ function ScalarForecastChecker({
   outcomeToken,
   forecast,
   amount,
+  receivedAmount,
+  collateralPerShare,
   selectedCollateral,
   isCollateralDai,
-  minPotentialReturn,
   daiToSDai,
   sDaiToDai,
 }: {
@@ -201,9 +203,10 @@ function ScalarForecastChecker({
   outcomeToken: Token;
   forecast: number;
   amount: string;
+  receivedAmount: number;
+  collateralPerShare: number;
   selectedCollateral: Token;
   isCollateralDai: boolean;
-  minPotentialReturn: number;
   daiToSDai: number;
   sDaiToDai: number;
 }) {
@@ -217,59 +220,251 @@ function ScalarForecastChecker({
     // fetchStatus: quoteFetchStatus,
     // error: quoteError,
   } = useQuoteTrade(market.chainId, zeroAddress, amount, otherOutcomeToken, selectedCollateral, "buy");
-
   if (getMarketType(market) !== MarketTypes.SCALAR || !quoteData) {
     return null;
   }
 
-  const receivedAmount = Number(formatUnits(quoteData.value, quoteData.decimals));
-  const collateralPerShare = getCollateralPerShare(
+  const otherReceivedAmount = Number(formatUnits(quoteData.value, quoteData.decimals));
+  const otherCollateralPerShare = getCollateralPerShare(
     "buy",
     selectedCollateral,
     quoteData,
     amount,
-    receivedAmount,
+    otherReceivedAmount,
     isCollateralDai,
     daiToSDai,
   );
 
-  // Find the token object that matches the buyToken address
-  const buyTokenIndex = outcomeTokens.findIndex((token) => isTwoStringsEqual(token.address, otherOutcomeToken.address));
-
-  const returnPerToken = getScalarReturnPerToken(market, buyTokenIndex, forecast);
-
-  const { potentialReturn } = getPotentialReturn(
-    collateralPerShare,
-    returnPerToken,
-    isCollateralDai,
-    receivedAmount,
-    sDaiToDai,
-    false,
+  const otherTokenIndex = outcomeTokens.findIndex((token) =>
+    isTwoStringsEqual(token.address, otherOutcomeToken.address),
+  );
+  const inputData = [
+    {
+      collateralPerShare,
+      receivedAmount,
+    },
+    {
+      collateralPerShare: otherCollateralPerShare,
+      receivedAmount: otherReceivedAmount,
+    },
+  ];
+  if (otherTokenIndex === 0) {
+    inputData.reverse();
+  }
+  const data = inputData.map(({ collateralPerShare, receivedAmount }, index) => {
+    const returnPerToken = getScalarReturnPerToken(market, index, forecast);
+    const { returnPercentage, potentialReturn } = getPotentialReturn(
+      collateralPerShare,
+      returnPerToken,
+      isCollateralDai,
+      receivedAmount,
+      sDaiToDai,
+      false,
+    );
+    return {
+      returnPerToken,
+      potentialReturn,
+      returnPercentage,
+    };
+  });
+  const renderReturnPerToken = (returnPercentage: number, returnPerToken: number) => (
+    <span className={clsx(returnPercentage >= 0 ? "text-success-primary" : "text-error-primary", "text-right")}>
+      {returnPerToken.toFixed(3)} {isCollateralDai ? "sDAI" : selectedCollateral.symbol}
+      {isCollateralDai ? ` (${(returnPerToken * (sDaiToDai ?? 0)).toFixed(3)} ${selectedCollateral.symbol})` : ""}
+    </span>
+  );
+  const renderPotentialReturn = (returnPercentage: number, potentialReturn: number) => (
+    <span className={clsx(returnPercentage >= 0 ? "text-success-primary" : "text-error-primary", "text-right")}>
+      {potentialReturn.toFixed(3)} {isCollateralDai ? selectedCollateral.symbol : "sDAI"} ({returnPercentage.toFixed(2)}
+      %)
+    </span>
   );
 
-  if (potentialReturn <= minPotentialReturn) {
-    return null;
+  const bestReturnIndex = data[1].potentialReturn > data[0].potentialReturn ? 1 : 0;
+  const maxPoints = 200;
+  const interval = Math.floor(Number(market.upperBound - market.lowerBound) / maxPoints);
+  const forecasts = Array(maxPoints)
+    .fill(null)
+    .map((_, index) => interval * index);
+  if (forecasts[forecasts.length - 1] !== Number(market.upperBound)) {
+    forecasts.push(Number(market.upperBound));
   }
+  const chartData = Array(2)
+    .fill(null)
+    .map((_, outcomeIndex) => {
+      const points = forecasts.map((forecast) => {
+        const returnPerToken = getScalarReturnPerToken(market, outcomeIndex, forecast);
+        const { potentialReturn } = getPotentialReturn(
+          inputData[outcomeIndex].collateralPerShare,
+          returnPerToken,
+          isCollateralDai,
+          inputData[outcomeIndex].receivedAmount,
+          sDaiToDai,
+          false,
+        );
+        return [forecast, potentialReturn];
+      });
 
-  if (buyTokenIndex === -1 || quoteIsLoading) {
+      return {
+        data: points,
+        name: market.outcomes[outcomeIndex],
+        type: "line",
+      };
+    });
+  const option = {
+    color: ["#00C42B", "#F60C36"],
+
+    tooltip: {
+      trigger: "axis",
+      axisPointer: {
+        type: "none",
+      },
+      valueFormatter: (value: number) => {
+        return `${value.toLocaleString()} ${isCollateralDai ? "sDAI" : selectedCollateral.symbol}`;
+      },
+    },
+
+    grid: {
+      left: 50,
+      right: 50,
+      top: "15%",
+      bottom: "15%",
+    },
+    xAxis: {
+      min: "dataMin",
+      max: "dataMax",
+      splitLine: {
+        show: false,
+      },
+      axisTick: {
+        alignWithLabel: true,
+        customValues: forecasts.filter((_: number, index: number) => index % Math.floor(forecasts.length / 5) === 0),
+      },
+      axisPointer: {
+        label: {
+          formatter: ({ value }: { value: number }) => `${value.toLocaleString()}`,
+        },
+      },
+
+      type: "value",
+      axisLabel: {
+        formatter: (value: number) => `${value.toLocaleString()}`,
+        customValues: forecasts.filter((_: number, index: number) => index % Math.floor(forecasts.length / 5) === 0),
+      },
+    },
+    yAxis: {
+      min: "dataMin",
+      max: "dataMax",
+      axisLabel: {
+        formatter: (value: number) => `${value.toLocaleString()}`,
+      },
+    },
+    series: [
+      ...[...chartData].reverse().map((x, index) => ({
+        ...x,
+        symbol: "circle",
+        symbolSize: 7,
+        showSymbol: false,
+        endLabel: {
+          show: true,
+          formatter: (params: { seriesName: string }) => params.seriesName,
+          color: index === 0 ? "#00C42B" : "#F60C36",
+        },
+      })),
+      {
+        name: "Mark Line",
+        type: "line",
+        data: [
+          [forecast, 0],
+          [forecast, Math.max(chartData[0].data[0][1], chartData[1].data.slice(-1)[0][1])],
+        ],
+        lineStyle: {
+          color: "#9747ff",
+          type: "dotted",
+          width: 2,
+        },
+        // smooth: true,
+        // silent: true,
+        tooltip: { show: false },
+        symbol: "none",
+        markLine: {
+          symbol: ["none", "none"],
+          lineStyle: {
+            color: "rgba(0, 0, 0, 0)",
+          },
+          label: {
+            show: true,
+            position: "end",
+            color: "#9747ff",
+            fontWeight: "bold",
+          },
+          data: [
+            [
+              {
+                name: "Current Forecast",
+                xAxis: forecast,
+                yAxis: 0,
+              },
+              { name: "end", xAxis: forecast, yAxis: "max" },
+            ],
+          ],
+        },
+      },
+    ],
+  };
+
+  if (otherTokenIndex === -1 || quoteIsLoading) {
     return null;
   }
 
   // find the return for each outcome token
   return (
-    <div>
-      Best return for your forecast: buy {market.outcomes[buyTokenIndex]}{" "}
-      <PotentialReturnResult
-        quoteIsLoading={quoteIsLoading}
-        isFetching={false}
-        isCollateralDai={isCollateralDai}
-        selectedCollateral={selectedCollateral}
-        receivedAmount={receivedAmount}
-        sDaiToDai={sDaiToDai ?? 0}
-        returnPerToken={returnPerToken}
-        collateralPerShare={collateralPerShare}
-        isOneOrNothingPotentialReturn={false}
-      />
+    <div className="text-[14px] space-y-6">
+      <div>
+        <div className="flex items-center py-3 border-b border-black-secondary">
+          <p className="w-[30%] font-semibold">Buying</p>
+          <p
+            className={clsx(
+              "w-[35%] text-right",
+              data[1].returnPercentage >= 0 ? "text-success-primary" : "text-error-primary",
+            )}
+          >
+            UP
+          </p>
+          <p
+            className={clsx(
+              "w-[35%] text-right",
+              data[0].returnPercentage >= 0 ? "text-success-primary" : "text-error-primary",
+            )}
+          >
+            DOWN
+          </p>
+        </div>
+        <div className="flex items-center py-3 border-b border-black-secondary">
+          <p className="w-[30%] font-semibold">Return per token:</p>
+          <p className="w-[35%] text-right">{renderReturnPerToken(data[1].returnPercentage, data[1].returnPerToken)}</p>
+          <p className="w-[35%] text-right">{renderReturnPerToken(data[0].returnPercentage, data[0].returnPerToken)}</p>
+        </div>
+        <div className="flex items-center py-3 border-b border-black-secondary">
+          <p className="w-[30%] font-semibold">Potential return:</p>
+          <p className="w-[35%] text-right">
+            {renderPotentialReturn(data[1].returnPercentage, data[1].potentialReturn)}
+          </p>
+          <p className="w-[35%] text-right">
+            {renderPotentialReturn(data[0].returnPercentage, data[0].potentialReturn)}
+          </p>
+        </div>
+      </div>
+      <div>
+        <ReactECharts option={option} key={bestReturnIndex} />
+      </div>
+      <div className="flex items-center flex-col">
+        <p className="font-semibold mb-2">Best return for your forecast</p>
+        <p className="text-success-primary">
+          Buy {market.outcomes[bestReturnIndex]}{" "}
+          {renderPotentialReturn(data[bestReturnIndex].returnPercentage, data[bestReturnIndex].potentialReturn)}
+        </p>
+      </div>
     </div>
   );
 }
@@ -314,29 +509,8 @@ function PotentialReturnConfig({
   const { sDaiToDai, daiToSDai } = useSDaiDaiRatio(market.chainId);
   const returnPerTokenDai = returnPerToken * (sDaiToDai ?? 0);
 
-  const { potentialReturn: minPotentialReturn } = getPotentialReturn(
-    collateralPerShare,
-    returnPerToken,
-    isCollateralDai,
-    receivedAmount,
-    sDaiToDai,
-    false,
-  );
-
-  const modalContent = (
-    <div className="space-y-2 w-full">
-      <p>Enter a possible market resolution to see your potential return.</p>
-      <p className="font-semibold text-purple-primary py-4">Current Outcome: {outcomeText}</p>
-      <div className="max-h-[200px] overflow-auto">
-        <RenderInputByMarketType
-          market={market}
-          outcomeToken={outcomeToken}
-          outcomeText={outcomeText}
-          input={input}
-          setInput={setInput}
-          setReturnPerToken={setReturnPerToken}
-        />
-      </div>
+  const potentialReturnContent = (
+    <div>
       <p>
         Return per token:{" "}
         <span className="font-semibold text-purple-primary">
@@ -358,19 +532,40 @@ function PotentialReturnConfig({
           isOneOrNothingPotentialReturn={false}
         />
       </div>
-      {input.scalar !== undefined && (
-        <ScalarForecastChecker
+    </div>
+  );
+
+  const scalarPotentialReturnContent = (
+    <ScalarForecastChecker
+      market={market}
+      outcomeToken={outcomeToken}
+      selectedCollateral={selectedCollateral}
+      forecast={input.scalar ?? 0}
+      amount={amount}
+      receivedAmount={receivedAmount}
+      collateralPerShare={collateralPerShare}
+      isCollateralDai={isCollateralDai}
+      daiToSDai={daiToSDai ?? 0}
+      sDaiToDai={sDaiToDai ?? 0}
+    />
+  );
+
+  const modalContent = (
+    <div className="space-y-2 w-full">
+      <p>Enter a possible market resolution to see your potential return.</p>
+      <p className="font-semibold text-purple-primary py-4">Current Outcome: {outcomeText}</p>
+      <div className="max-h-[200px] overflow-auto">
+        <RenderInputByMarketType
           market={market}
           outcomeToken={outcomeToken}
-          selectedCollateral={selectedCollateral}
-          forecast={input.scalar}
-          amount={amount}
-          isCollateralDai={isCollateralDai}
-          minPotentialReturn={minPotentialReturn}
-          daiToSDai={daiToSDai ?? 0}
-          sDaiToDai={sDaiToDai ?? 0}
+          outcomeText={outcomeText}
+          input={input}
+          setInput={setInput}
+          setReturnPerToken={setReturnPerToken}
         />
-      )}
+      </div>
+
+      {getMarketType(market) === MarketTypes.SCALAR ? scalarPotentialReturnContent : potentialReturnContent}
       <div className="text-center pt-4">
         <Button type="button" variant="primary" size="small" text="Close" onClick={closeModal} />
       </div>
