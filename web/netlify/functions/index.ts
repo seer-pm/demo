cbbfbe854fa6be878686500355ba4158dac68f51
xyzseer <xyzseer@gmail.com>
2025-05-13 12:02:49 -0300

    add durable cache directive

--- web/netlify/functions/index.ts
+++ web/netlify/functions/index.ts
@@ -4,8 +4,17 @@ export default async (req: Request) => {
   const pageContext = await renderPage({ urlOriginal: req.url });
   if (!pageContext.httpResponse) return new Response("", { status: 200 });
 
+  const headers = {
+    ...Object.fromEntries(pageContext.httpResponse.headers),
+  };
+
+  // Add cache control only for market pages
+  if (req.url.includes("/markets/")) {
+    headers["Netlify-CDN-Cache-Control"] = "public, durable, max-age=60, stale-while-revalidate=120";
+  }
+
   return new Response(pageContext.httpResponse.body, {
     status: pageContext.httpResponse.statusCode,
-    headers: Object.fromEntries(pageContext.httpResponse.headers),
+    headers,
   });
 };
