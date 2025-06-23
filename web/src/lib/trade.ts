f87eb8b1de8e4601e9c25c1b44075f4ef534a6de
xyzseer <xyzseer@gmail.com>
2025-06-23 02:01:46 +0200

    swapr trade: remove plimit

--- web/src/lib/trade.ts
+++ web/src/lib/trade.ts
@@ -12,11 +12,8 @@ import {
   TradeType,
   UniswapTrade,
 } from "@swapr/sdk";
-import pLimit from "p-limit";
 import { Address, parseUnits, zeroAddress } from "viem";
 import { Token } from "./tokens";
-
-const limit = pLimit(1);
 
 export interface QuoteTradeResult {
   value: bigint;
@@ -63,16 +60,12 @@ export function getSwaprTrade(
   account: Address | undefined,
   _chainId: number,
 ): Promise<SwaprV3Trade | null> {
-  return limit(async () => {
-    const quoteResult = await SwaprV3Trade.getQuote({
-      amount: currencyAmountIn,
-      quoteCurrency: currencyOut,
-      maximumSlippage,
-      recipient: account || zeroAddress,
-      tradeType: TradeType.EXACT_INPUT,
-    });
-    await new Promise((res) => setTimeout(res, 300));
-    return quoteResult;
+  return SwaprV3Trade.getQuote({
+    amount: currencyAmountIn,
+    quoteCurrency: currencyOut,
+    maximumSlippage,
+    recipient: account || zeroAddress,
+    tradeType: TradeType.EXACT_INPUT,
   });
 }
 
