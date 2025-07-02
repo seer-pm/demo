c1173fad9135673f52fc5e3f123735f0fd655ae9
nvm1410 <vominhbinto@gmail.com>
2025-07-02 11:11:56 +0700

    test

--- web/src/pages/markets/@chainId/@id/+onBeforePrerenderStart.ts
+++ web/src/pages/markets/@chainId/@id/+onBeforePrerenderStart.ts
@@ -33,7 +33,17 @@ async function fetchCharts(): Promise<Record<Address, PoolHourDatasSets>> {
 
 export default async function onBeforePrerenderStart() {
   try {
-    const markets = await fetchMarkets();
+    const markets = await fetchMarkets({
+      chainsList: [],
+      marketName: "",
+      marketStatusList: [],
+      creator: "",
+      participant: "",
+      orderBy: undefined,
+      orderDirection: undefined,
+      marketIds: undefined,
+      disabled: undefined,
+    });
     const charts = await fetchCharts();
     // biome-ignore lint/suspicious/noExplicitAny:
     const data: { url: string; pageContext: any }[] = markets
