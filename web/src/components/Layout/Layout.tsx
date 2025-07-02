ecb9a02dd662e93b878ab02b7a52c96fb9b5205e
nvm1410 <vominhbinto@gmail.com>
2025-07-02 10:10:50 +0700

    test

--- web/src/components/Layout/Layout.tsx
+++ web/src/components/Layout/Layout.tsx
@@ -1,16 +1,9 @@
 import Footer from "@/components/Layout/Footer";
 import Header from "@/components/Layout/Header";
 import LayoutShell from "@/components/Layout/LayoutShell";
-import { GNOSIS_RPC } from "@/wagmi";
-import { ChainId, configureRpcProviders } from "@swapr/sdk";
-import React, { useEffect } from "react";
+import React from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    configureRpcProviders({
-      [ChainId.XDAI]: GNOSIS_RPC,
-    });
-  }, []);
   return (
     // <Suspense>
     <LayoutShell>
