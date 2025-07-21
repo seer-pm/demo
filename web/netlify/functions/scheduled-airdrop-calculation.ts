d8b2a7f2e209eb75f0ddde51a8c6b2bf5d93412f
nvm1410 <vominhbinto@gmail.com>
2025-07-21 19:20:20 +0700

    update cronjob interval

--- web/netlify/functions/scheduled-airdrop-calculation.ts
+++ web/netlify/functions/scheduled-airdrop-calculation.ts
@@ -8,14 +8,14 @@ export default async (req: Request) => {
   try {
     const { next_run } = await req.json();
     console.log("Received event! Next invocation at:", next_run);
-    // await fetch(
-    //   process.env.AIRDROP_CALCULATION_URL ?? "http://app.seer.pm/.netlify/functions/airdrop-calculation-background",
-    // );
+    await fetch(
+      process.env.AIRDROP_CALCULATION_URL ?? "http://app.seer.pm/.netlify/functions/airdrop-calculation-background",
+    );
   } catch (e) {
     console.log(e);
   }
 };
 
 export const config: Config = {
-  schedule: "0 * * * *",
+  schedule: "0 */4 * * *",
 };
