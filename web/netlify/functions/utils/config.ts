7e2df3aca233c0045805ae6ea10b17e8b7c7c50c
xyzseer <xyzseer@gmail.com>
2025-08-28 10:24:06 -0300

    fix get market by chainId

--- web/netlify/functions/utils/config.ts
+++ web/netlify/functions/utils/config.ts
@@ -5,12 +5,13 @@ export { gnosis, mainnet, optimism, base };
 
 // TODO: replace this
 export const config = createConfig({
-  chains: [mainnet, sepolia, gnosis, optimism],
+  chains: [mainnet, sepolia, gnosis, optimism, base],
   transports: {
     [mainnet.id]: http(process.env.PRIVATE_RPC_MAINNET || "https://eth-pokt.nodies.app"),
     [sepolia.id]: http(process.env.PRIVATE_RPC_SEPOLIA || "https://ethereum-sepolia-rpc.publicnode.com"),
     [gnosis.id]: http(process.env.PRIVATE_RPC_GNOSIS || "https://gnosis-pokt.nodies.app"),
     [optimism.id]: http(process.env.PRIVATE_RPC_OPTIMISM || "https://mainnet.optimism.io"),
+    [base.id]: http(process.env.PRIVATE_RPC_BASE || "https://base.llamarpc.com"),
   },
 });
 
