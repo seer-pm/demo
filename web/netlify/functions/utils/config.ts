cc72810fa08380b840d3c0f8102bbb14bf04ed00
xyzseer <xyzseer@gmail.com>
2025-09-10 10:55:10 -0300

    fix netlify functions rpc urls

--- web/netlify/functions/utils/config.ts
+++ web/netlify/functions/utils/config.ts
@@ -1,17 +1,30 @@
+import { ChainId, configureRpcProviders } from "@swapr/sdk";
 import { http, createConfig } from "wagmi";
 import { base, gnosis, mainnet, optimism, sepolia } from "wagmi/chains";
 
 export { gnosis, mainnet, optimism, base };
 
+const GNOSIS_RPC = process.env.PRIVATE_RPC_GNOSIS || "https://gnosis-pokt.nodies.app";
+const MAINNET_RPC = process.env.PRIVATE_RPC_MAINNET || "https://eth-pokt.nodies.app";
+const OPTIMISM_RPC = process.env.PRIVATE_RPC_OPTIMISM || "https://mainnet.optimism.io";
+const BASE_RPC = process.env.PRIVATE_RPC_BASE || "https://base.llamarpc.com";
+
+configureRpcProviders({
+  [ChainId.XDAI]: GNOSIS_RPC,
+  [ChainId.MAINNET]: MAINNET_RPC,
+  [ChainId.OPTIMISM_MAINNET]: OPTIMISM_RPC,
+  [ChainId.BASE]: BASE_RPC,
+});
+
 // TODO: replace this
 export const config = createConfig({
   chains: [mainnet, sepolia, gnosis, optimism, base],
   transports: {
-    [mainnet.id]: http(process.env.PRIVATE_RPC_MAINNET || "https://eth-pokt.nodies.app"),
+    [mainnet.id]: http(MAINNET_RPC),
     [sepolia.id]: http(process.env.PRIVATE_RPC_SEPOLIA || "https://ethereum-sepolia-rpc.publicnode.com"),
-    [gnosis.id]: http(process.env.PRIVATE_RPC_GNOSIS || "https://gnosis-pokt.nodies.app"),
-    [optimism.id]: http(process.env.PRIVATE_RPC_OPTIMISM || "https://mainnet.optimism.io"),
-    [base.id]: http(process.env.PRIVATE_RPC_BASE || "https://base.llamarpc.com"),
+    [gnosis.id]: http(GNOSIS_RPC),
+    [optimism.id]: http(OPTIMISM_RPC),
+    [base.id]: http(BASE_RPC),
   },
 });
 
