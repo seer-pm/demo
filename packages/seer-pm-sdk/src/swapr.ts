c71a6a2e2b11e5f336f96bbfc3802cc78b5997ad
xyzseer <xyzseer@gmail.com>
2026-03-31 16:05:50 +0200

    fix: add missing files

---
+++ packages/seer-pm-sdk/src/swapr.ts
@@ -0,0 +1,12 @@
+export {
+  ChainId,
+  configureRpcProviders,
+  TradeType,
+  Trade,
+  CoWTrade,
+  SwaprV3Trade,
+  UniswapTrade,
+  Currency,
+  CurrencyAmount,
+  Percent,
+} from "@swapr/sdk";
